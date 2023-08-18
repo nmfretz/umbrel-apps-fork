a5adab72d4494950fcc5af8bbc26a09282c07ed8
nmfretz <nmfretz@gmail.com>
2023-08-18 12:41:06 -0700

    testy-mcTestface

--- portainer/exports.sh
+++ portainer/exports.sh
@@ -1,2 +1,2 @@
-export DEVICE_IP=$(ip -4 addr show docker0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-echo "DEVICE_IP=$DEVICE_IP"
+export DOCKER_HOST_IP=$(ip -4 addr show docker0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+echo "DOCKER_HOST_IP=$DOCKER_HOST_IP"
