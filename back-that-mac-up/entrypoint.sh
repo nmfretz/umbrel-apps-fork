97f76b99dd7a78ec1dfb8d91e0653e83a770007d
nmfretz <nmfretz@gmail.com>
2023-12-04 21:47:30 -0800

    refactor for no-slider

--- back-that-mac-up/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# `s6-svscanctl -t /etc/s6` for clean shutdown: https://skarnet.org/software/s6/s6-svscanctl.html
-nc -l -p 50000 -e sh -c "s6-svscanctl -t /etc/s6" &
-
-# Could also just pkill the process:
-# nc -l -p 50000 -e pkill s6-svscan &
-
-exec /entrypoint.sh "$@"
