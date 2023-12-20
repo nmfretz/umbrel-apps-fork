28899d058bc3f312e8cfa0c6362fb806efe3bc2c
nmfretz <nmfretz@gmail.com>
2023-12-20 14:53:09 -0800

    add echo logs for testing

--- invoice-ninja/exports.sh
+++ invoice-ninja/exports.sh
@@ -1,5 +1,6 @@
 LARAVEL_APP_KEY_FILE_PATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}")/..)/data/laravel-app-key.txt"
-
+echo "LARAVEL_APP_KEY_FILE_PATH: ${LARAVEL_APP_KEY_FILE_PATH}"
 if [[ -f "${LARAVEL_APP_KEY_FILE_PATH}" ]]; then
   export APP_KEY=$(cat "${LARAVEL_APP_KEY_FILE_PATH}")
+  echo "APP_KEY: ${APP_KEY}"
 fi
