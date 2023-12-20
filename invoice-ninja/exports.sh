6d5addf93f8fe89b6eb3a372fb621aefb346acaf
nmfretz <nmfretz@gmail.com>
2023-12-20 14:56:44 -0800

    fix exports path

--- invoice-ninja/exports.sh
+++ invoice-ninja/exports.sh
@@ -1,4 +1,4 @@
-LARAVEL_APP_KEY_FILE_PATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}")/..)/data/laravel-app-key.txt"
+LARAVEL_APP_KEY_FILE_PATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))/data/laravel-app-key.txt"
 echo "LARAVEL_APP_KEY_FILE_PATH: ${LARAVEL_APP_KEY_FILE_PATH}"
 if [[ -f "${LARAVEL_APP_KEY_FILE_PATH}" ]]; then
   export APP_KEY=$(cat "${LARAVEL_APP_KEY_FILE_PATH}")
