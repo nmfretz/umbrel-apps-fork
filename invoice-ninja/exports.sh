7a6d78a27241b0c1a127306d7562737d8d0e85b1
nmfretz <nmfretz@gmail.com>
2023-12-21 10:32:40 -0800

    trim

--- invoice-ninja/exports.sh
+++ invoice-ninja/exports.sh
@@ -1,5 +1,5 @@
 LARAVEL_APP_KEY_FILE_PATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))/data/laravel-app-key.txt"
 
 if [[ -f "${LARAVEL_APP_KEY_FILE_PATH}" ]]; then
-  export APP_KEY=$(cat "${LARAVEL_APP_KEY_FILE_PATH}")
+  export APP_KEY=$(cat "${LARAVEL_APP_KEY_FILE_PATH}" | tr -d '\r\n')
 fi
