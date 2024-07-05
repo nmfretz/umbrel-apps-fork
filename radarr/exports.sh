1932286da628e288a981a01f157e39426531e9ec
nmfretz <nmfretz@gmail.com>
2024-07-05 10:03:56 +1000

    temp commit

---
+++ radarr/exports.sh
@@ -0,0 +1,14 @@
+# Check if qBittorrent or SABnzbd are installed
+installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
+
+if echo "$installed_apps" | grep --quiet 'qbittorrent'; then
+  export APP_RADARR_QBITTORRENT_INSTALLED="true"
+fi
+
+if echo "$installed_apps" | grep --quiet 'sabnzbd'; then
+  export APP_RADARR_SABNZBD_INSTALLED="true"
+fi
+
+# export SABNZBD_API_KEY, which has the format
+# api_key = 98e3444f7fab45e592958673bf656g3
+export SABNZBD_API_KEY=$(grep -Po 'api_key = \K.*' "${UMBREL_ROOT}/app-data/sabnzbd/data/config/sabnzbd.ini")
